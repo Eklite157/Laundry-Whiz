6910669a9012c767695b69506c6d29afa2ab5047
irene chen <autumnspringbreeze@gmail.com>
2025-01-25 16:15:22 -0500

    made a button that does nothing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
 import LaundryCard from "@/components/LaundryCard";
+
+function handleClick() {
+  console.log('something is happening');
+}
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
- Whats up, hello there
+
+    <button onClick={handleClick}>this is a button</button>
+
+  Whats up, hello there
 <LaundryCard />
 <LaundryCard />
 <LaundryCard />
