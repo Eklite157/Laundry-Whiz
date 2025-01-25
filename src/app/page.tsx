45aaf99b09dd83379aedef88b2f66611a2acbf41
irene chen <autumnspringbreeze@gmail.com>
2025-01-25 16:58:31 -0500

    functioning button!!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
 "use client";
+import { useState } from 'react';
 import LaundryCard from "@/components/LaundryCard";
 
-function handleClick() {
-  console.log("something is happening");
-}
-
 export default function Home() {
+  const [in_use, set_in_use] = useState("no");
+
+  function handleClick() {
+    if (in_use == "no") {
+      set_in_use("yes")
+    } else {
+      set_in_use("no");
+    }
+  }
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
-    <button onClick={handleClick()}>"this is a button"</button>
+    <button onClick={handleClick}>this machine is in use: {in_use}</button>
 
   Whats up, hello there
 <LaundryCard />
