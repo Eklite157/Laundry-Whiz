54eda0449f5b636b0c800f3b59a503bb98eaa155
irene chen <autumnspringbreeze@gmail.com>
2025-01-25 16:20:08 -0500

    barely any changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@
 import LaundryCard from "@/components/LaundryCard";
 
 function handleClick() {
-  console.log('something is happening');
+  console.log("something is happening");
 }
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
-    <button onClick={handleClick}>this is a button</button>
+    <button onClick={handleClick()}>"this is a button"</button>
 
   Whats up, hello there
 <LaundryCard />
