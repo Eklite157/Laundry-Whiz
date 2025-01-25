476d81e0919542ef28d323a31664ff3a9dcaba7f
Eklite157 <violley12@gmail.com>
2025-01-25 18:45:42 -0500

    Laundry room+ timer+ machines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,22 +30,39 @@ export default function Home() {
       }
     }, 1000);
   }
-  
+
+
   return (
     <>
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div>
+
+    <h1> Laundry Room </h1>
 
     <button onClick={handleClick}>this machine is in use: {in_use}</button>
-
     <p>there are {timer} seconds left</p>
 
-  Whats up, hello there
-<LaundryCard />
-<LaundryCard />
-<LaundryCard />
-<LaundryCard />
-<LaundryCard />
-<LaundryCard />
+  
+    <div className="laundry-container">
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+
+    </div>
 
     </div>
     </>)
--- src/components/LaundryCard.tsx
+++ src/components/LaundryCard.tsx
@@ -1,6 +1,6 @@
 export default function LaundryCard() {
     return <div>
         <img src="/images/LaundryMachine.png" width={300} height={200}/>
-        Laundry Machine</div>
-}
+        Laundry Machine</div>}
+
 
