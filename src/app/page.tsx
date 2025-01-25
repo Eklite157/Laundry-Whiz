aeaf5e077d1bfd30759e14ac75e72e547fd41d10
irene chen <autumnspringbreeze@gmail.com>
2025-01-25 18:43:23 -0500

    available in use

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,16 @@ import "./css/laundryroom.css"
 
 export default function Home() {
   const laundry_time = 5;
-  const [in_use, set_in_use] = useState("no");
+  const [in_use, set_in_use] = useState("Available");
   let [timer, set_timer] = useState(laundry_time);
 
   function handleClick() {
-    if (in_use == "no") {
-      set_in_use("yes");
+    if (in_use == "Available") {
+      set_in_use("In Use");
       set_timer(laundry_time);
       startTimer();
     } else {
-      set_in_use("no");
+      set_in_use("Available");
     }
   }
 
@@ -26,7 +26,7 @@ export default function Home() {
         set_timer(timer--);
       } else {
         console.log("timer < 89 timer is", timer);
-        set_timer(laundry_time);
+        set_timer(0);
       }
     }, 1000);
   }
@@ -35,7 +35,7 @@ export default function Home() {
     <>
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
-    <button onClick={handleClick}>this machine is in use: {in_use}</button>
+    <button onClick={handleClick}>{in_use}</button>
 
     <p>there are {timer} seconds left</p>
 
