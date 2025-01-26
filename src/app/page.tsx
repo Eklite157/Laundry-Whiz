f8d50421fd5c868c72012e035c2f81c3302982f0
irene chen <autumnspringbreeze@gmail.com>
2025-01-25 21:59:10 -0500

    tryna do stuff make sure to change var = true to test not auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,49 @@
 "use client";
 import LaundryCard from "@/components/LaundryCard";
 import "./css/laundryroom.css"
-
+//import "./css/loginpage.css"
 
 export default function Home() {
 
-  return (
-    <>
+  var auth = false;
+  if (!auth) {
+    return (
+      <>
+      <h1> Login Page </h1>
 
-    <h1> Laundry Room </h1>
+      <div className="login-page">
+        <p> Username: </p>
 
-  
-    <div className="laundry-room">
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
-    <LaundryCard />
+      </div>
+      </>
+    )
+  } else {
+    return (
+      <>
+      <h1> Laundry Room </h1>
 
-    </div>
-    </>)
+      <div className="laundry-room">
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+      <LaundryCard />
+
+      </div>
+      </>)
+  }
 }
 
