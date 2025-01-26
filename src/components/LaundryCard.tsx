4a403eb0145d49ed55f5b6e2e6e2798571139234
irene chen <autumnspringbreeze@gmail.com>
2025-01-25 20:34:29 -0500

    previous working code

--- src/components/LaundryCard.tsx
+++ src/components/LaundryCard.tsx
@@ -2,32 +2,34 @@
 import { useState } from 'react';
 
 export default function LaundryCard() {
-    const laundry_time = 5;
-    const [in_use, set_in_use] = useState("Available");
-    let [timer, set_timer] = useState(laundry_time);
+  const laundry_time = 3;
+  const [in_use, set_in_use] = useState("Available");
+  let [timer, set_timer] = useState(laundry_time);
 
-    function handleClick() {
-        if (in_use == "Available") {
-          set_in_use("In Use");
-          set_timer(laundry_time);
-          startTimer();
-        } else {
-          set_in_use("Available");
-        }
+  function handleClick() {
+    if (in_use == "Available") {
+      set_in_use("In Use");
+      set_timer(laundry_time);
+      startTimer();
+    } else {
+      set_in_use("Available");
+    }
+  }
+
+
+  //start timer for specific machine 
+
+  function startTimer() {
+    setInterval(() => {
+      if (timer > 0) {
+        console.log("timer > 89 timer is", timer);
+        set_timer(timer--);
+      } else {
+        console.log("timer < 89 timer is", timer);
+        set_timer(0);
       }
-      
-      //start timer for specific machine 
-      function startTimer() {
-          setInterval(() => {
-            if (timer > 0) {
-              console.log("timer > 89 timer is", timer);
-              set_timer(timer--);
-            } else {
-              console.log("timer < 89 timer is", timer);
-              set_timer(0);
-            }
-          }, 1000);
-        }
+    }, 1000);
+  }
 
     return <div className="laundry-machine">
         Laundry Machine
