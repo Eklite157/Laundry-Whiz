8f5b498bcf51af4677f114e56cf61a97065018ee
Eklite157 <violley12@gmail.com>
2025-01-25 20:16:02 -0500

    changed laundry card

--- src/components/LaundryCard.tsx
+++ src/components/LaundryCard.tsx
@@ -1,6 +1,39 @@
+"use client";
+import { useState } from 'react';
+
 export default function LaundryCard() {
+    const laundry_time = 5;
+    const [in_use, set_in_use] = useState("Available");
+    let [timer, set_timer] = useState(laundry_time);
+
+    function handleClick() {
+        if (in_use == "Available") {
+          set_in_use("In Use");
+          set_timer(laundry_time);
+          startTimer();
+        } else {
+          set_in_use("Available");
+        }
+      }
+      
+      //start timer for specific machine 
+      function startTimer() {
+          setInterval(() => {
+            if (timer > 0) {
+              console.log("timer > 89 timer is", timer);
+              set_timer(timer--);
+            } else {
+              console.log("timer < 89 timer is", timer);
+              set_timer(0);
+            }
+          }, 1000);
+        }
+
     return <div>
+        Laundry Machine
         <img src="/images/LaundryMachine.png" width={300} height={200}/>
-        Laundry Machine</div>}
+        <button className="btn" onClick={handleClick}>{in_use}</button>
+        <figcaption className="timer-notice">there are {timer} seconds left</figcaption>
+        </div>}
 
 
