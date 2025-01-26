9bb98b408f1dfce94a30c8384fe5e161a84c76c7
Eklite157 <violley12@gmail.com>
2025-01-25 20:15:44 -0500

    Added buttons to every machine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,14 @@
 "use client";
-import { useState } from 'react';
 import LaundryCard from "@/components/LaundryCard";
 import "./css/laundryroom.css"
 
 
 export default function Home() {
-  const laundry_time = 5;
-  const [in_use, set_in_use] = useState("Available");
-  let [timer, set_timer] = useState(laundry_time);
-
-  function handleClick() {
-    if (in_use == "Available") {
-      set_in_use("In Use");
-      set_timer(laundry_time);
-      startTimer();
-    } else {
-      set_in_use("Available");
-    }
-  }
-
-
-  //start timer for specific machine 
-
-  function startTimer() {
-    setInterval(() => {
-      if (timer > 0) {
-        console.log("timer > 89 timer is", timer);
-        set_timer(timer--);
-      } else {
-        console.log("timer < 89 timer is", timer);
-        set_timer(0);
-      }
-    }, 1000);
-  }
-
 
   return (
     <>
 
     <h1> Laundry Room </h1>
-
-    <button className="btn" onClick={handleClick}>{in_use}</button>
-
-    <p className="timer-notice">there are {timer} seconds left</p>
 
   
     <div className="laundry-container">
