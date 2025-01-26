bc580040a09b227ce46e6bf52baf5bc4f35252b5
Eklite157 <violley12@gmail.com>
2025-01-25 19:53:58 -0500

    should be good now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       set_timer(laundry_time);
       startTimer();
     } else {
-      set_in_use("");
+      set_in_use("Available");
     }
   }
 
@@ -33,23 +33,40 @@ export default function Home() {
       }
     }, 1000);
   }
-  
+
+
   return (
     <>
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+
+    <h1> Laundry Room </h1>
 
     <button onClick={handleClick}>this machine is in use: {in_use}</button>
+    <button onClick={handleClick}>{in_use}</button>
 
     <p>there are {timer} seconds left</p>
 
-  Whats up, hello there
-<LaundryCard />
-<LaundryCard />
-<LaundryCard />
-<LaundryCard />
-<LaundryCard />
-<LaundryCard />
+  
+    <div className="laundry-container">
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
+    <LaundryCard />
 
     </div>
     </>)
 }
+
