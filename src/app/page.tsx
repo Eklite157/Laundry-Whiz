ea6b66dfd580e7ae55b0257becb85bc5cc2a55fd
Eklite157 <violley12@gmail.com>
2025-01-25 19:40:47 -0500

    aa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,8 +19,9 @@ export default function Home() {
     }
   }
 
+
+  //start timer for specific machine 
   
-  //start timer for specific machine
   function startTimer() {
     setInterval(() => {
       if (timer > 0) {
